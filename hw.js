05cd51f63990c5e601d525b19d753c828c6620f6
John Hanna <john497dit@hotmail.com>
2021-01-13 19:05:20 -0600

    test

---
+++ hw.js
@@ -0,0 +1,15 @@
+
+
+    let date_ob = new Date();
+
+// adjust 0 before single digit date
+let date = ("0" + date_ob.getDate()).slice(-2);
+
+// current month
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+// current year
+let year = date_ob.getFullYear();
+
+// prints date in YYYY-MM-DD format
+document.write(month + "-"+ date+"-"+year);
